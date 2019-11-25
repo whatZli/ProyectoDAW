a1b4a0c625d17bea7fb2213ad7b76aff50396ffa
Alex Dominguez <alexander.domdom@educa.jcyl.es>
2019-11-25 19:32:11 +0100

    Borrado de carpetas innecesarias

--- public_html/proyectoDWES/proyectoTema4/mostrarcodigo/ejercicio03Pdo.php
+++
@@ -1,8 +0,0 @@
-<?php require '../core/cabecera.php';?>
-   <div class="content" style="text-align:left; background-color:white; font-size:15px;">
-    <?php
-    $nombreFichero=basename($_SERVER['PHP_SELF']);
-    
-    highlight_file("../codigoPHP/".$nombreFichero);
-    
-    require '../core/pie.php';
