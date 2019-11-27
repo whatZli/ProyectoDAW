02fe55e0599767725d09e00b954c426d69b826f4
Alex <Alex@DESKTOP-98GDN0D>
2019-11-27 23:23:00 +0100

    Ejercicio crónometro con reloj analógico hecho mejorar apariencia, falta terminar una cosa

--- proyectoDWEC/tema5/cronometro/reloj.js
+++ proyectoDWEC/tema5/cronometro/reloj.js
@@ -1,28 +1,28 @@
 var intervalo, horasC, minutosC, segundosC, miliSegundosC;
-var gradosH,gradosM,gradosS,gradosMS;
+var gradosH, gradosM, gradosS, gradosMS;
 var horas, minutos, segundos;
-var agujaMiliSegundosC,agujaSegundosC,agujaMinutosC,agujaHorasC;
+var agujaMiliSegundosC, agujaSegundosC, agujaMinutosC, agujaHorasC;
 
-function empezarCronometro() {   
-    horasC.style.color="#221F1F";
-    minutosC.style.color="#221F1F";
-    segundosC.style.color="#221F1F";
-    
+function empezarCronometro() {
+    horasC.style.color = "#221F1F";
+    minutosC.style.color = "#221F1F";
+    segundosC.style.color = "#221F1F";
+
     $ms = "00";
-    $s = "50";
-    $m = "59";
-    $h = "06";
-    $gradosS=0;
-    $gradosM=0;
-    $gradosH=0;
+    $s = "00";
+    $m = "00";
+    $h = "00";
+    $gradosS = 0;
+    $gradosM = 0;
+    $gradosH = 0;
     horasC.innerHTML = $h + " :";
     minutosC.innerHTML = $m + " :";
     segundosC.innerHTML = $s + " ";
     miliSegundosC.innerHTML = $ms;
-    
-    gradosH.innerHTML=$gradosH;
-    gradosM.innerHTML=$gradosM;
-    gradosS.innerHTML=$gradosS;
+
+    gradosH.innerHTML = $gradosH;
+    gradosM.innerHTML = $gradosM;
+    gradosS.innerHTML = $gradosS;
     intervalo = setInterval(function () {
         if ($ms === 100) {
             $ms = 00;
@@ -33,18 +33,18 @@ function empezarCronometro() {
             } else {
                 segundosC.innerHTML = $s + " ";
             }
-            gradosS.innerHTML=6*$s;
+            gradosS.innerHTML = 6 * $s;
             /*Cambiar agujas*/
-            agujaSegundosC.style.transform="rotate("+Math.round(6*$s)+"deg)";
-            agujaMinutosC.style.transform="rotate("+Math.round(6*$m)+"deg)";
-            agujaHorasC.style.transform="rotate("+Math.round(30*$h+0.5*$m)+"deg)";
+            agujaSegundosC.style.transform = "rotate(" + Math.round(6 * $s) + "deg)";
+            agujaMinutosC.style.transform = "rotate(" + Math.round(6 * $m) + "deg)";
+            agujaHorasC.style.transform = "rotate(" + Math.round(30 * $h + 0.5 * $m) + "deg)";
             if ($s === 60) {
                 $s = 0;
-                gradosS.innerHTML=6*$s;
+                gradosS.innerHTML = 6 * $s;
                 segundosC.innerHTML = "0" + $s;
                 $m++;
-                gradosM.innerHTML=6*$m;
-                gradosH.innerHTML=(30*$h+0.5*$m);
+                gradosM.innerHTML = 6 * $m;
+                gradosH.innerHTML = (30 * $h + 0.5 * $m);
                 if ($m < 10) {
                     minutosC.innerHTML = "0" + $m + " :";
                 } else {
@@ -53,10 +53,10 @@ function empezarCronometro() {
             }
             if ($m === 60) {
                 $m = 0;
-                gradosM.innerHTML=6*$m;
+                gradosM.innerHTML = 6 * $m;
                 minutosC.innerHTML = "0" + $m + " :";
                 $h++;
-                gradosH.innerHTML=(30*$h+0.5*$m);
+                gradosH.innerHTML = (30 * $h + 0.5 * $m);
                 if ($h < 10) {
                     horasC.innerHTML = "0" + $h + " :";
                 } else {
@@ -64,26 +64,26 @@ function empezarCronometro() {
                 }
             }
         } else {
-            if($ms<10){
-                miliSegundosC.innerHTML = "0"+$ms;
-            }else{
+            if ($ms < 10) {
+                miliSegundosC.innerHTML = "0" + $ms;
+            } else {
                 miliSegundosC.innerHTML = $ms;
             }
             //Es 3.6 porque hay que dividir el total de grados(360)
             //entre el número de milisegundos(100)
             //Cada segundo son 3.6 grados
-            gradosMS.innerHTML=Math.round(3.6*$ms);
-            agujaMiliSegundosC.style.transform="rotate("+Math.round(3.6*$ms)+"deg)";
+            gradosMS.innerHTML = Math.round(3.6 * $ms);
+            agujaMiliSegundosC.style.transform = "rotate(" + Math.round(3.6 * $ms) + "deg)";
             $ms++;
         }
     }, 10);
 }
 function pararCronometro() {
     clearInterval(intervalo);
-    horasC.style.color="#E50914";
-    minutosC.style.color="#E50914";
-    segundosC.style.color="#E50914";
-    
+    horasC.style.color = "#E50914";
+    minutosC.style.color = "#E50914";
+    segundosC.style.color = "#E50914";
+
 }
 function actualizarHora() {
     var d = new Date();
@@ -121,14 +121,19 @@ function cargar() {
     empezar.addEventListener('click', empezarCronometro, false);
     parar.addEventListener('click', pararCronometro, false);
     /*Cronómetro ----- grados*/
-    gradosH=document.getElementById("gradosH");
-    gradosM=document.getElementById("gradosM");
-    gradosS=document.getElementById("gradosS");
-    gradosMS=document.getElementById("gradosMS");
+    gradosH = document.getElementById("gradosH");
+    gradosM = document.getElementById("gradosM");
+    gradosS = document.getElementById("gradosS");
+    gradosMS = document.getElementById("gradosMS");
     /*Relog Analógico Cronómetro*/
-    agujaSegundosC=document.getElementById("agujaSegundosC");
-    agujaMiliSegundosC=document.getElementById("agujaMiliSegundosC");
-    agujaMinutosC=document.getElementById("agujaMinutosC");
-    agujaHorasC=document.getElementById("agujaHorasC");
+    agujaSegundosC = document.getElementById("agujaSegundosC");
+    agujaMiliSegundosC = document.getElementById("agujaMiliSegundosC");
+    agujaMinutosC = document.getElementById("agujaMinutosC");
+    agujaHorasC = document.getElementById("agujaHorasC");
+
+    /*Mover agujas al principio*/
+    agujaSegundosC.style.transform = "rotate(1080deg)";
+    agujaMinutosC.style.transform = "rotate(720deg)";
+    agujaHorasC.style.transform = "rotate(360deg)";
 }
 window.addEventListener('load', cargar, false);
